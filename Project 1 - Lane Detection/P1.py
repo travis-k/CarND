c4270365c194b3e64de39a5adb9e1781b1d3fccc
travis-k <tkrebs@ryerson.ca>
2017-02-19 01:24:41 -0500

    Finished basic project

--- Project 1 - Lane Detection/P1.py
+++ Project 1 - Lane Detection/P1.py
@@ -3,85 +3,163 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2
 import tensorflow as tf
+import math
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+
+## Helper Functions
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img
+
+# Python 3 has support for cool math symbols.
+
+def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + λ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, λ)
+
+def process_image(img):
+
+    gray = grayscale(img)
+    
+    # Define a kernel size and apply Gaussian smoothing
+    kernel_size = 11
+    blur_gray = gaussian_blur(img, kernel_size)
+    
+    # Define our parameters for Canny and apply
+    low_threshold = 60
+    high_threshold = 120
+    edges = canny(img, low_threshold, high_threshold)
+    
+    # Next we'll create a masked edges image using cv2.fillPoly()
+    mask = np.zeros_like(edges)   
+    ignore_mask_color = 255   
+    
+    # We define a triangle for our polygon mask
+    imshape = img.shape
+    vertices = np.array([[(0,imshape[0]),(imshape[1]/2, imshape[0]/1.9), (imshape[1],imshape[0])]], dtype=np.int32)
+    
+    masked_edges = region_of_interest(edges, vertices)
+    
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 5 # distance resolution in pixels of the Hough grid
+    theta = np.pi/90 # angular resolution in radians of the Hough grid
+    threshold = 50     # minimum number of votes (intersections in Hough grid cell)
+    min_line_len = 10 #minimum number of pixels making up a line
+    max_line_gap = 1    # maximum gap in pixels between connectable line segments
+    
+    # Run Hough on edge detected image
+    line_image = hough_lines(masked_edges, rho, theta, threshold, min_line_len, max_line_gap)
+    
+    # Create a "color" binary image to combine with line image
+    color_edges = np.dstack((edges, edges, edges)) 
+    
+    # Draw the lines on the edge image
+    marked_lane = weighted_img(line_image, img)#, α=0.8, β=1., λ=0.)
+    #plt.imshow(marked_lane)
+    
+    return marked_lane
+
+## Main Functions
 
 # Loading test images
-strImage = 'test_images/solidWhiteCurve.jpg'
-image = mpimg.imread(strImage)
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# strImage = 'test_images/solidWhiteCurve.jpg'
+# strImage = 'test_images/solidWhiteRight.jpg'
+# strImage = 'test_images/solidYellowCurve.jpg'
+# strImage = 'test_images/solidYellowCurve2.jpg'
+# strImage = 'test_images/solidYellowLeft.jpg'
+strImage = 'test_images/whiteCarLaneSwitch.jpg'
 
-# Define a kernel size and apply Gaussian smoothing
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+# img = mpimg.imread(strImage)
 
-# Define our parameters for Canny and apply
-low_threshold = 50
-high_threshold = 150
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+# marked_lane = process_image(img)
 
-# Next we'll create a masked edges image using cv2.fillPoly()
-mask = np.zeros_like(edges)   
-ignore_mask_color = 255   
-
-# This time we are defining a four sided polygon to mask
-imshape = image.shape
-vertices = np.array([[(0,imshape[0]),(imshape[1]/2, imshape[0]/2), (imshape[1]/2, imshape[0]/2), (imshape[1],imshape[0])]], dtype=np.int32)
-cv2.fillPoly(mask, vertices, ignore_mask_color)
-masked_edges = cv2.bitwise_and(edges, mask)
-
-# Define the Hough transform parameters
-# Make a blank the same size as our image to draw on
-rho = 5 # distance resolution in pixels of the Hough grid
-theta = np.pi/90 # angular resolution in radians of the Hough grid
-threshold = 40     # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 15 #minimum number of pixels making up a line
-max_line_gap = 1    # maximum gap in pixels between connectable line segments
-line_image = np.copy(image)*0 # creating a blank to draw lines on
-
-# Run Hough on edge detected image
-lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]),
-                            min_line_length, max_line_gap)
-
-# Iterate over the output "lines" and draw lines on the blank
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
-
-# Create a "color" binary image to combine with line image
-color_edges = np.dstack((edges, edges, edges)) 
-
-# Draw the lines on the edge image
-combo = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
-plt.imshow(combo)
-
-# # ### Test OpenCV
-# 
-# import cv2
-# 
-# gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-# plt.imshow(gray, cmap='Greys_r')
-# 
-# import tensorflow as tf
-# 
-# with tf.Session() as sess:
-#     a = tf.constant(1)
-#     b = tf.constant(2)
-#     c = a + b
-#     # Should be 3
-#     print("1 + 2 = {}".format(sess.run(c)))
-# 
-# 
-# 
-# # Import everything needed to edit/save/watch video clips
-# from moviepy.editor import VideoFileClip
-# from IPython.display import HTML
-# 
-# 
-# # Create a new video with `moviepy` by processing each frame to [YUV](https://en.wikipedia.org/wiki/YUV) color space.
-# 
-# new_clip_output = 'test_output.mp4'
-# test_clip = VideoFileClip("test.mp4")
-# new_clip = test_clip.fl_image(lambda x: cv2.cvtColor(x, cv2.COLOR_RGB2YUV)) #NOTE: this function expects color images!!
-# new_clip.write_videofile(new_clip_output, audio=False)
+white_output = 'white.mp4'
+clip1 = VideoFileClip("solidWhiteRight.mp4")
+white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip.write_videofile(white_output, audio=False)
 
 
 
