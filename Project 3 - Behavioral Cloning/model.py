c65babea20095fc746932703dfeed3f1aa5fd921
travis-k <tkrebs@ryerson.ca>
2017-04-18 01:01:17 -0400

    Cleaning up directory

--- Project 3 - Behavioral Cloning/model.py
+++ Project 3 - Behavioral Cloning/model.py
@@ -15,7 +15,7 @@ from sklearn.utils import shuffle
 import keras
 print(keras.__version__)
 
-def generator(samples, batch_size=32):
+def generator(samples, bias = 1.0, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -27,12 +27,15 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
 
                 # Chosing randomly whether left or right
-                img_choice = np.random.randint(3)                
-                name = 'IMG\\' + batch_sample[img_choice].split('/')[-1]                
+                img_choice = np.random.randint(3) 
+                # img_choice = 0               
+                name = 'IMG\\' + batch_sample[img_choice].split('/')[-1]    
+                # name = 'IMG_mine\\' + batch_sample[img_choice].split('\\')[-1]             
                 img = cv2.imread(name)
+                
                 angle = float(batch_sample[3])
                 
-                correction = 0.25
+                correction = 0.1
                 
                 if img_choice == 1:
                     angle += correction
@@ -44,8 +47,10 @@ def generator(samples, batch_size=32):
                     img = np.fliplr(img)
                     angle = -angle
                 
-                images = append(img)
-                angles = append(angle)
+                threshold = np.random.uniform()
+                if (abs(angle) + bias) >= threshold or angles == []:
+                    images.append(img)
+                    angles.append(angle)
                 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -56,6 +61,7 @@ def generator(samples, batch_size=32):
 
 samples = []
 with open('./driving_log.csv') as csvfile:
+# with open('./driving_log_mine.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -71,8 +77,6 @@ model.compile(loss='mse', optimizer='adam')
 
 ## Train the model
 history_object = model.fit_generator(train_generator, steps_per_epoch=200, validation_data=validation_generator, validation_steps=200, epochs=8, verbose=1)
-
-# model.load_weights('model_weights.h5')
 
 a = next(train_generator)
 b = model.predict(a[0])
--- Project 3 - Behavioral Cloning/squeezenet.py
+++ Project 3 - Behavioral Cloning/squeezenet.py
@@ -1,51 +1,10 @@
 from keras.applications.imagenet_utils import _obtain_input_shape
 from keras import backend as K
 from keras.layers import Input, Convolution2D, MaxPooling2D, Activation, concatenate, Dropout, GlobalAveragePooling2D, warnings, Dense, Flatten
-<<<<<<< HEAD
 from keras.models import Model, Sequential
 from keras.layers import Cropping2D, Lambda, BatchNormalization
 from keras.layers.advanced_activations import ELU
-=======
-from keras.models import Model
-from keras.layers import Cropping2D, Lambda
->>>>>>> refs/remotes/origin/master
-# from keras.engine.topology import get_source_inputs
-# from keras.utils import get_file
-# from keras.utils import layer_utils
 
-sq1x1 = "squeeze1x1"
-exp1x1 = "expand1x1"
-exp3x3 = "expand3x3"
-relu = "relu_"
-
-WEIGHTS_PATH = "https://github.com/rcmalli/keras-squeezenet/releases/download/v1.0/squeezenet_weights_tf_dim_ordering_tf_kernels.h5"
-
-# Modular function for Fire Node
-
-def fire_module(x, fire_id, squeeze=16, expand=64):
-    s_id = 'fire' + str(fire_id) + '/'
-
-    if K.image_data_format() == 'channels_first':
-        channel_axis = 1
-    else:
-        channel_axis = 3
-    
-    x = Convolution2D(squeeze, (1, 1), padding='valid', name=s_id + sq1x1)(x)
-    x = Activation('relu', name=s_id + relu + sq1x1)(x)
-
-    left = Convolution2D(expand, (1, 1), padding='valid', name=s_id + exp1x1)(x)
-    left = Activation('relu', name=s_id + relu + exp1x1)(left)
-
-    right = Convolution2D(expand, (3, 3), padding='same', name=s_id + exp3x3)(x)
-    right = Activation('relu', name=s_id + relu + exp3x3)(right)
-
-    x = concatenate([left, right], axis=channel_axis, name=s_id + 'concat')
-    return x
-
-
-# Original SqueezeNet from paper.
-
-<<<<<<< HEAD
 def squeezenet():
     
     input_shape=(160,320,3)
@@ -69,7 +28,7 @@ def squeezenet():
     # Initiating the model
     model = Sequential()
     
-    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=input_shape))
     # model.add(Lambda(lambda x: (x / 255.0) - 0.5))  
     model.add(BatchNormalization(axis=1)) 
     
@@ -118,126 +77,3 @@ def squeezenet():
     model.add(Dense(nb_classes))
     
     return(model)
-    
-    
-    # if weights not in {'imagenet', None}:
-    #     raise ValueError('The `weights` argument should be either '
-    #                      '`None` (random initialization) or `imagenet` '
-    #                      '(pre-training on ImageNet).')
-
-   ##   if weights == 'imagenet' and classes != 1000:
-    #     raise ValueError('If using `weights` as imagenet with `include_top`'
-    #                      ' as true, `classes` should be 1000')
-
-   ##   img_input = Input(shape=(160,320,3))
-
-   ##   ch, row, col = 3, 80, 320  # Trimmed image format
-    # x = Cropping2D(cropping=((50,20), (0,0)))(img_input)
-    # x = Lambda(lambda y: y/127.5 - 1., output_shape=(row, col, ch))(x)
-
-   ##   x = Convolution2D(64, (3, 3), strides=(2, 2), padding='valid', name='conv1')(img_input)
-    # x = Activation('relu', name='relu_conv1')(x)
-    # x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
-
-   ##   x = fire_module(x, fire_id=2, squeeze=16, expand=64)
-    # x = fire_module(x, fire_id=3, squeeze=16, expand=64)
-    # x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
-
-   ##   x = fire_module(x, fire_id=4, squeeze=32, expand=128)
-    # x = fire_module(x, fire_id=5, squeeze=32, expand=128)
-    # x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
-
-   ##   x = fire_module(x, fire_id=6, squeeze=48, expand=192)
-    # x = fire_module(x, fire_id=7, squeeze=48, expand=192)
-    # x = fire_module(x, fire_id=8, squeeze=64, expand=256)
-    # x = fire_module(x, fire_id=9, squeeze=64, expand=256)
-    # x = Dropout(0.5, name='drop9')(x)
-    # 
-    # x = Convolution2D(100, (1, 1), padding='valid', name='conv10')(x)
-    # x = Activation('relu', name='relu_conv10')(x)
-    # 
-    # x = Flatten()(x)
-    # x = Dense(128, name='dense_1')(x)
-    # x = Activation('relu')(x)
-    # 
-    # x = Dense(64, name='dense_2')(x)
-    # x = Activation('relu')(x)
-    # 
-    # x = Dense(16, name='dense_3')(x)
-    # x = Activation('relu')(x)
-    # 
-    # out = Dense(1, name='dense_out')(x)
-    # # x = GlobalAveragePooling2D()(x)
-    # # out = Activation('softmax', name='loss')(x)
-
-   ##   model = Model(img_input, out, name='squeezenet')
-
-   ##   return model
-=======
-def squeezenet(input_shape=None, weights=None):
-    
-    if weights not in {'imagenet', None}:
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization) or `imagenet` '
-                         '(pre-training on ImageNet).')
-
-    if weights == 'imagenet' and classes != 1000:
-        raise ValueError('If using `weights` as imagenet with `include_top`'
-                         ' as true, `classes` should be 1000')
-
-    img_input = Input(shape=(160,320,3))
-
-    ch, row, col = 3, 80, 320  # Trimmed image format
-    x = Cropping2D(cropping=((50,20), (0,0)))(img_input)
-    x = Lambda(lambda y: y/127.5 - 1., output_shape=(row, col, ch))(x)
-
-    x = Convolution2D(64, (3, 3), strides=(2, 2), padding='valid', name='conv1')(img_input)
-    x = Activation('relu', name='relu_conv1')(x)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool1')(x)
-
-    x = fire_module(x, fire_id=2, squeeze=16, expand=64)
-    x = fire_module(x, fire_id=3, squeeze=16, expand=64)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool3')(x)
-
-    x = fire_module(x, fire_id=4, squeeze=32, expand=128)
-    x = fire_module(x, fire_id=5, squeeze=32, expand=128)
-    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), name='pool5')(x)
-
-    x = fire_module(x, fire_id=6, squeeze=48, expand=192)
-    x = fire_module(x, fire_id=7, squeeze=48, expand=192)
-    x = fire_module(x, fire_id=8, squeeze=64, expand=256)
-    x = fire_module(x, fire_id=9, squeeze=64, expand=256)
-    x = Dropout(0.5, name='drop9')(x)
-
-    x = Convolution2D(100, (1, 1), padding='valid', name='conv10')(x)
-    x = Activation('relu', name='relu_conv10')(x)
-    x = Flatten()(x)
-    out = Dense(1, name='dense_out')(x)
-    # x = GlobalAveragePooling2D()(x)
-    # out = Activation('softmax', name='loss')(x)
-
-    model = Model(img_input, out, name='squeezenet')
-
-    # load weights
-    if weights == 'imagenet':
-
-        weights_path = get_file('squeezenet_weights_tf_dim_ordering_tf_kernels.h5',
-                                    WEIGHTS_PATH,
-                                    cache_subdir='models')
-        model.load_weights(weights_path)
-        if K.backend() == 'theano':
-            layer_utils.convert_all_kernels_in_model(model)
-
-        if K.image_data_format() == 'channels_first':
-
-            if K.backend() == 'tensorflow':
-                warnings.warn('You are using the TensorFlow backend, yet you '
-                              'are using the Theano '
-                              'image data format convention '
-                              '(`image_data_format="channels_first"`). '
-                              'For best performance, set '
-                              '`image_data_format="channels_last"` in '
-                              'your Keras config '
-                              'at ~/.keras/keras.json.')
-    return model
->>>>>>> refs/remotes/origin/master
