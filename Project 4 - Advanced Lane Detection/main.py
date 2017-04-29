59197322ccd631bf250f71dc96d8062f1d9bba41
travis-k <tkrebs@ryerson.ca>
2017-04-29 18:12:38 -0400

    Working on pipeline in main.py

--- Project 4 - Advanced Lane Detection/main.py
+++ Project 4 - Advanced Lane Detection/main.py
@@ -8,6 +8,32 @@ import os
 import scipy.misc
 import pickle
 
+def pipeline(img, s_thresh, sx_thresh):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hsv[:,:,1]
+    s_channel = hsv[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary ==1) | (sxbinary == 1)] = 1
+    return combined_binary
+
 def warper(img, src, dst):
     # Compute and apply perpective transform
     img_size = (img.shape[1], img.shape[0])
@@ -15,6 +41,24 @@ def warper(img, src, dst):
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
     return warped
 
+def region_of_interest(img, vertices):
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (1,) * channel_count
+    else:
+        ignore_mask_color = 1
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+    
 def camera_calibration(strCalibrationIn, img_size):
     
     # prepare object points
@@ -81,13 +125,14 @@ if plot_it == True:
 
 ## Reading in Test Images
 # plot_it = True
-# plot_it = False
+plot_it = False
 
 strTestIn = ["test_images/" + x for x in os.listdir("test_images/")]
 # Getting rid of "thumbs.db" on my machine
 if "test_images/Thumbs.db" in strTestIn: strTestIn.remove("test_images/Thumbs.db")
 
-img = cv2.imread(strTestIn[1]) # Test image
+# Undistorting test image
+img = cv2.imread(strTestIn[3]) # Test image
 dst = cv2.undistort(img, mtx, dist, None, mtx) # Undistorted test image
 
 if plot_it == True:
@@ -96,14 +141,125 @@ if plot_it == True:
     ax1.set_title('Original Image', fontsize=30)
     ax2.imshow(dst)
     ax2.set_title('Undistorted Image', fontsize=30)
+
+# Applying colour transformation
+s_thresh=(170, 255)
+sx_thresh=(20, 100)
+colour_binary = pipeline(dst, s_thresh=s_thresh, sx_thresh=sx_thresh)
+
+if plot_it == True:
+    # Plot the result
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
     
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=40)
+    
+    ax2.imshow(colour_binary)
+    ax2.set_title('Pipeline Result', fontsize=40)
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 
+# Mask
+img_size = shape(colour_binary)
+height_offset = 300; # How the height of the warped image will change
+lr_padding = 200; # Left-right padding 
+ratio = (1060-270)/(680-605) # Ratio of horizon straight edge to hood straight edge
 
+# This unmodified box fits well around the lanes on a flat straight road
+src = np.float32([[670+round(lr_padding/ratio), 440],[1060+lr_padding, 675],[250-lr_padding, 675],[610-round(lr_padding/ratio), 440]])
+dest = np.float32([[1060+lr_padding, 440-height_offset],[1060+lr_padding, 675],[270-lr_padding, 675],[270-lr_padding, 440-height_offset]])
+colour_binary_masked = region_of_interest(colour_binary, np.int32([src]))
 
+# Warp it
+warped = warper(colour_binary_masked, src, dest)
 
+# # if plot_it == True:
+# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+# ax1.imshow(img)
+# ax1.plot(src[:,0],src[:,1],'.-r')
+# ax1.plot(src[0:4:3,0],src[0:4:3,1],'.-r')
+# ax1.set_title('Original Image', fontsize=30)
+# ax2.imshow(warped)
+# ax2.set_title('Warped Image', fontsize=30)
 
+# window settings
+window_width = 50 
+window_height = 80 # Break image into 9 vertical layers since image height is 720
+margin = 100 # How much to slide left and right for searching
 
+def window_mask(width, height, img_ref, center,level):
+    output = np.zeros_like(img_ref)
+    output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height),max(0,int(center-width/2)):min(int(center+width/2),img_ref.shape[1])] = 1
+    return output
 
+def find_window_centroids(image, window_width, window_height, margin):
+    
+    window_centroids = [] # Store the (left,right) window centroid positions per level
+    window = np.ones(window_width) # Create our window template that we will use for convolutions
+    
+    # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
+    # and then np.convolve the vertical image slice with the window template 
+    
+    # Sum quarter bottom of image to get slice, could use a different ratio
+    l_sum = np.sum(warped[int(3*warped.shape[0]/4):,:int(warped.shape[1]/2)], axis=0)
+    l_center = np.argmax(np.convolve(window,l_sum))-window_width/2
+    r_sum = np.sum(warped[int(3*warped.shape[0]/4):,int(warped.shape[1]/2):], axis=0)
+    r_center = np.argmax(np.convolve(window,r_sum))-window_width/2+int(warped.shape[1]/2)
+    
+    # Add what we found for the first layer
+    window_centroids.append((l_center,r_center))
+    
+    # Go through each layer looking for max pixel locations
+    for level in range(1,(int)(warped.shape[0]/window_height)):
+        # convolve the window into the vertical slice of the image
+        image_layer = np.sum(warped[int(warped.shape[0]-(level+1)*window_height):int(warped.shape[0]-level*window_height),:], axis=0)
+        conv_signal = np.convolve(window, image_layer)
+        # Find the best left centroid by using past left center as a reference
+        # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
+        offset = window_width/2
+        l_min_index = int(max(l_center+offset-margin,0))
+        l_max_index = int(min(l_center+offset+margin,warped.shape[1]))
+        l_center = np.argmax(conv_signal[l_min_index:l_max_index])+l_min_index-offset
+        # Find the best right centroid by using past right center as a reference
+        r_min_index = int(max(r_center+offset-margin,0))
+        r_max_index = int(min(r_center+offset+margin,warped.shape[1]))
+        r_center = np.argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
+        # Add what we found for that layer
+        window_centroids.append((l_center,r_center))
 
+    return window_centroids
 
+window_centroids = find_window_centroids(warped, window_width, window_height, margin)
+
+# If we found any window centers
+if len(window_centroids) > 0:
+
+    # Points used to draw all the left and right windows
+    l_points = np.zeros_like(warped)
+    r_points = np.zeros_like(warped)
+
+    # Go through each level and draw the windows 	
+    for level in range(0,len(window_centroids)):
+        # Window_mask is a function to draw window areas
+        l_mask = window_mask(window_width,window_height,warped,window_centroids[level][0],level)
+        r_mask = window_mask(window_width,window_height,warped,window_centroids[level][1],level)
+        # Add graphic points from window mask here to total pixels found 
+        l_points[(l_points == 255) | ((l_mask == 1) ) ] = 255
+        r_points[(r_points == 255) | ((r_mask == 1) ) ] = 255
+
+    # Draw the results
+    template = np.array(r_points+l_points,np.uint8) # add both left and right window pixels together
+    zero_channel = np.zeros_like(template) # create a zero color channel
+    template = np.array(cv2.merge((zero_channel,template,zero_channel)),np.uint8) # make window pixels green
+    warpage = np.array(cv2.merge((warped,warped,warped)),np.uint8) # making the original road pixels 3 color channels
+    output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0) # overlay the orignal road image with window results
+ 
+# If no window centers found, just display orginal road image
+else:
+    output = np.array(cv2.merge((warped,warped,warped)),np.uint8)
+
+# Display the final results
+plt.imshow(output)
+plt.title('window fitting results')
+plt.show()
 
